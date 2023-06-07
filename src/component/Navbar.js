85803db2a82f8f852f16810cb874addbed5c1e6e
siblarn <siblarn5905@gmail.com>
2023-06-07 23:15:27 +0700

    fix example01 Navbar & Header

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,20 @@
+import React from "react"
+import logo from '../logo.svg';
+
 export default function Navbar (prop) {
     let text = prop.text || ""
     return (
-        <header>
-            
-        <div>
-            <h3>ReactFacts</h3>
+        <>
+        <div id="wer">
 
+
+            <img src={logo} width="75"  alt="logo" />
+            <nav style={{display:"flex",width:"100%",justifyContent:"space-between"}}>ReactFacts
+
+                <div className="nav1">React Course - Project 1</div>
+
+            </nav>
         </div>
-        <div>
-
-            <h4>React Course - Project 1</h4>
-
-        </div>
-        </header>
+        </>    
     )
 }
