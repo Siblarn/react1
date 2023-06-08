c5f6219b025852bd91b12802197b293e49a7af5a
siblarn <siblarn5905@gmail.com>
2023-06-08 09:50:24 +0700

    fix-แก้Header-Footer -นำheaderที่ไม่ใช้งานออก

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,7 @@ export default function Footer (prop) {
     const text = prop.text || ""
     return (
                 <footer>
-                    {/* <small>© 2021 Ziroll development. All rights reserved.</small> */}
+                    <small>© 2021 Ziroll development. All rights reserved.</small>
                 </footer>
     
         
