be9e4d7c003398b5646bf995b3f2f52e8f4a0e18
siblarn <siblarn5905@gmail.com>
2023-06-08 09:51:13 +0700

    นำolไม่ได้ใช้ออก-ปรับmargin

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import Navbar from './component/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <Navbar/>
       <Header/>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -3,10 +3,10 @@ export default function Header (prop) {
     return (
         <header>
             {/* <h2>Reasons I'm excited to learn React</h2> */}
-              <ol>
+              <>
                 {/* <li>It's a popular library, so I'll be able to fit in with the cool kids!</li> */}
                 {/* <li>I'm more likely to get a job as a developer if I know React</li> */}
-              </ol>
+              </>
             <h1>Fun Facts About React</h1>
               <ul>
                 <li>Was first released in 2013 </li>
