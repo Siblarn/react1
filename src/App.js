30fe64349949cdb0c32120c10b0b43ad96b39556
siblarn <siblarn5905@gmail.com>
2023-06-09 17:14:13 +0700

    fix resume

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 // import ReactFact from "./pages/FirstReact";
 // import { Route, Routes, useParams } from "react-router-dom";
-import Navbar from "./component/Navbar";
-import Body2 from "./pages/component/Body2";
-import Card from "./pages/component/Card";
+// import Navbar from "./component/Navbar";
+// import Body2 from "./pages/component/Body2";
+// import Card from "./pages/component/Card";
+import Resume from "./Resume";
 
 function App() {
   return (
     <div className="App">
 
-    
-      <Navbar />
-      <Card/>
-      <Body2 name='test'/>
+      <Resume/>
+      {/* <Navbar /> */}
+      {/* <Card/> */}
+      {/* <Body2 name='test'/> */}
       {/* <Header/> */}
       {/* <Footer/> */}
+
     </div>
   );
 }
