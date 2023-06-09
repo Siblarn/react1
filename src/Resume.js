a98fc72677ec104fd4b876771940ce2471d321a9
siblarn <siblarn5905@gmail.com>
2023-06-09 17:10:48 +0700

    example Resume

---
+++ src/Resume.js
@@ -0,0 +1,85 @@
+import React from "react";
+import S from "./Image/S.png";
+
+export default function Resume (){
+    return (
+        <>
+            <div className="header"style={{margin:'30px'}}>
+            
+                <div style={{display:'flex'}}>
+                    <div className="MyImage" style={{margin:'30px'}} ></div>
+                        <img src={S} width="260" alt="text"></img>
+                
+                    <div className="h1" style={{margin:'50px',fontWeight:'800'}}>
+                        <div style={{fontWeight:'500',fontSize:'35px'}}>Thippawan Puttachart</div>
+                        <div>_____________________________________________________________________________________________________________________________</div>
+                        <br></br>
+                        <div style={{display:'flex'}}>
+                        <div style={{fontWeight:'900'}}>Info </div>
+                        <div style={{fontWeight:'400',padding:'0px 70px'}}>Female(22year)</div>
+                    </div>
+                        
+                        <div className="h2" style={{display:'flex'}}>
+                            <div>Address</div>
+                            <div style={{padding:'0px 39px',fontWeight:'400'}}>51/1 Soi Pracha Uthit 54, Bang Mod, Thung Khru District, Bangkok 10140</div>
+                        </div>
+                        <div className="h3" style={{display:'flex'}}>
+                            <div>E-mail</div>
+                            <div style={{marginLeft:'54px',fontWeight:'400'}}>siblarn5905@gmail.com</div>
+                            <div style={{marginLeft:'30px'}}>Tel</div>
+                            <div style={{fontWeight:'400',marginLeft:'30px'}}>0929985905</div>
+                        </div>  
+                </div>
+            </div>  
+        </div>
+        <div>
+                    <div className="personal" style={{marginLeft:'150px',fontWeight:'550',fontSize:'20px',display:'flex',borderRight:'20px'}}>
+                        <div>PERSONAL DETAIL</div>
+                        <div style={{marginLeft:'190px'}}>EDUCATION</div>
+                        <div style={{borderRight:'20px'}}></div>
+
+                    </div>
+                
+                    <div className="nickname" style={{marginLeft:'150px',lineHeight:'55px',display:'flex'}}>
+                        <div style={{fontWeight:'800'}}>Nickname</div>
+                        <div style={{marginLeft:'60px'}}>Siblarn</div>
+                        <div style={{marginLeft:'210px',fontSize:'19px'}}>king mongkut's university of technology thonburi</div>                        <br></br>
+                    </div>
+                    <div className="age" style={{display:'flex',marginLeft:'150px'}}>
+                        <div style={{fontWeight:'800'}}>Age</div>
+                        <div style={{marginLeft:'106px'}}>22</div>
+                        <div style={{marginLeft:'285px'}}>Faculty of industrial education and technology.</div>
+                    </div>
+                    <div className="nationnality"style={{display:'flex',marginLeft:'150px',lineHeight:'55px'}}>
+                        <div style={{fontWeight:'800'}}>Nationnality</div>
+                        <div style={{marginLeft:'39px'}}>Thai</div>
+                        <div style={{marginLeft:'300px'}}>Department of Electrical engineering.</div>
+
+                    </div>
+                    <div className="language" style={{display:'flex',marginLeft:'150px',lineHeight:'28px'}}>
+                        <div style={{fontWeight:'800'}}>Language</div>
+                        <div style={{marginLeft:'60px'}}>Thai</div>
+                        <div style={{marginLeft:'320px'}}>Major of Computer engineering</div>
+                    </div>
+                    <div className="personallity"style={{display:'flex',marginLeft:'150px',lineHeight:'55px'}}>
+                        <div style={{fontWeight:'800'}}>Personallity</div>
+                        <div style={{marginLeft:'43px'}}>Friendly</div>
+                        <div style={{marginLeft:'370px'}}>GPA : 3.00</div>
+                        
+                    </div>
+        
+        </div>
+            <div className=""style={{}}>
+                    <div className="experience">
+                        <div style={{display:'flex'}}>
+                        <div style={{fontWeight:'750',fontSize:'22px',marginLeft:'195px',lineHeight:'100px'}}>EXPERIENCE</div>
+                        <div style={{alignItems:'center'}}></div>
+                        </div>
+                        <li style={{marginLeft:'190px'}}>Tido Tech Co.</li>
+                    </div>
+
+            </div>
+        </>        
+        
+        )
+    }
