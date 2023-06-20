348013f406150d4104c038b887cf8972deff12b0
siblarn <siblarn5905@gmail.com>
2023-06-20 16:50:47 +0700

    ToDolist

--- src/App.js
+++ src/App.js
@@ -1,21 +1,120 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} width="150" className="App-logo" alt="logo" />
-        <h1>Reasons I'm excited to learn React</h1>
-        <ol>
-                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
-      </header>
-    </div>
+
+    const [todos, setTodos] = useState(() => {
+      const savedTodos = localStorage.getItem("todos");
+      if (savedTodos) {
+        return JSON.parse(savedTodos);
+      } else {
+        return [];
+      }
+    });
+    const [todo, setTodo] = useState("");
+    useEffect(() => {
+      localStorage.setItem("todos",JSON.stringify(todos));
+    },[todos]);
+  
+    const [isEditting, setisEditing] = useState(false);
+    const [currentTodo, setCurrenTodo] = useState({});
+  
+    function handleEditInPutChange(e) {
+      setCurrenTodo({ ...currentTodo, text: e.target.value });
+      console.log("Current Todo ", currentTodo);
+    }
+  
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+    function handleInputChange(e) {
+      setTodo(e.target.value);
+    }
+  
+    function handleFromSubmit(e) {
+      e.preventDefault();
+      if (todo !== "") {
+        setTodos([
+          ...todos,
+          {
+            id: todo.length + 1,
+            text: todo.trim(),
+          },
+        ]);
+      }
+      setTodo("");
+    }
+  
+    function handleDeleteClick(id) {
+      const removeItem = todos.filter((todo) => {
+        return todo.id !== id
+      })
+      setTodos(removeItem);
+    }
+  
+    function handleEditChange(todo) {
+      setisEditing(true);
+      setCurrenTodo({ ...todo })
+    }
+  
+    function handleUpdateTodo(id, updateTodo) {
+      const updatedItem = todos.map((todo) => {
+        return todo.id === id ? updateTodo : todo;
+      });
+  
+      setisEditing(false);
+      setTodos(updatedItem);
+    }
+  
+    function handleEditFromSubmit(e) {
+      e.preventDefault();
+  
+      handleUpdateTodo(currentTodo.id, currentTodo);
+    }
+  
+    console.log(todos)
+  
+    return (
+      <div className="App">
+        <h1>todolists</h1>
+        {isEditting ? (
+          <from onSubmit={handleEditFromSubmit}>
+            <h2>Edit Todo</h2>
+            <label htmFor="editTodo">Edit todo: </label>
+            <input
+              type="text"
+              name="editTodo"
+              placeholder="Edit todo"
+              value={currentTodo.text}
+              onChange={handleEditInPutChange}
+            />
+            <button type="submit">Update</button>
+            <button onClick={() => setisEditing(false)}>Cancel</button>
+          </from>
+        ) : (
+          <from onSubmit={handleFromSubmit}>
+            <input
+              type="text"
+              name="todo"
+              placeholder="create a new to do"
+              value={todo}
+              onChange={handleInputChange}
+            />
+            <button type="submit">Add</button>
+          </from>
+        )}
+        <ul className="todo-lish">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+        
+              <button onClick={() => handleEditClick(todo)}>Edit</button>
+              <button onClick={() => handleDeleteClick(todo.id)}>x</button>
+            </li>
+          ))}
+        </ul>
+        </div>
   );
 }
 
