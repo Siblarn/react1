8a1c905d3e5133d5dce16200a5504e4b2e54b4dd
siblarn <siblarn5905@gmail.com>
2023-06-21 15:41:50 +0700

    เพิ่มedit/checkbox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const preTodo = [{ id: 1, text: "Helloworld !", createAt: Date.now() }];
+  const preTodo = [{ id: 1, text: "Assign 4", createAt: Date.now() }];
   const [todo, setTodo] = useState(preTodo);
   const [input, setInput] = useState("");
 
@@ -19,12 +19,18 @@ function App() {
     const addTodo = [...todo, preAddTodo];
     setTodo(addTodo);
   }
-
   function handleDelete(index) {
     const filteredArr = todo.filter((obj) => obj.id !== index);
     return setTodo(filteredArr);
   }
+  const [checked, setChecked] = useState(false);
+  function handleChange(e, checkbox) {
+    setTodo(e.target.value);
 
+    // checkboxes.forEach((item) => {
+    //   item !== checkbox ? setChecked(false) : setChecked(true);
+    // });
+  }
   // function handleFromSubmit() {
   //   if (todo !== "") {
   //     setTodo([
@@ -66,41 +72,130 @@ function App() {
 
   // console.log(todos);
 
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleFrom = (e) => {
+    setIsEditing(!isEditing);
+  };
+  const handleClick = (todo) => {
+    remove(e.target.preTodo);
+    if (isEditing) {
+    } else {
+    }
+  };
+ 
+
+
+  
   return (
     <div className="App">
-      <h1>todolists</h1>
-      <input
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
-      <button
-        type="submit"
-        onClick={() => {
-          handleAdd();
-        }}
-      >
-        Add
-      </button>
+    
+        <div
+          className="nav1"
+          style={{
+            width: "350px",
+            height: "250px",
+            fontSize: "18px",
+            fontWeight: "600",
+            borderRadius: "25px 25px 25px 25px",
+            borderTop: "4px solid silver",
+            borderBottom: "4px solid silver",
+          }}
+        >
+          <div style={{ marginLeft: "6.5rem" }}>
+            <div style={{ fontSize: "30px" }}> To Do Lists</div>
+          </div>
 
-      {/* </from> */}
-      <ul className="todo-lish">
-        {todo &&
-          todo.map((todo, index) => (
-            <li key={todo.id}>
-              {todo.text}
+          <input
+            style={{
+              backgroundColor: "#efb2fb",
+              border: "3px solid #ff008450",
+              width: "160px",
+              height: "16px",
+              borderRadius: "4px 4px 4px 4px",
+              marginLeft: "10px",
+            }}
+            value={input}
+            placeholder="New to do list."
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <button
+            style={{
+              backgroundColor: "#ff008450",
+              border: "3px solid #ff008450",
+              fontWeight: "700",
+              borderRadius: "4px 4px 4px 4px",
+              marginLeft: "3px",
+            }}
+            type="submit"
+            onClick={() => {
+              handleAdd();
+            }}
+          >
+            Add
+          </button>
+
+          <div className="clickcolumn">
+            {/* </from> */}
+            <ul className="todo-lish">
+              {todo &&
+                todo.map((todo, index) => (
+                  <div key={todo.id}>
+                    {todo.text}
+
+                    <button
+                      style={{
+                        backgroundColor: "#ff008450",
+                        border: "3.2px solid #b98cce",
+                        color: "red",
+                        fontWeight: "800",
+                        borderRadius: "7px 7px 7px 7px",
+                      }}
+                      onClick={() => {
+                        handleDelete(todo.id);
+                      }}
+                    >
+                      X
+                    </button>
+                    <label key={todo}>
+                      <input type="checkbox" />
+                      <span className="check">{""}</span>
+                    </label>
+                  </div>
+                ))}
+            </ul>
+          </div>
+          <div className="Todo">
+            <form className="Todo-edit-form" onSubmit={handleClick}>
+              <input
+                onChange={handleClick}
+                value={input}
+                placeholder="Edit to do list."
+                style={{
+                  backgroundColor: "#efb2fb",
+                  border: "3px solid #ff008450",
+                  width: "160px",
+                  height: "16px",
+                  borderRadius: "4px 4px 4px 4px",
+                  marginLeft: "10px",
+                }}
+              />
               <button
-                onClick={() => {
-                  handleDelete(todo.id);
+                style={{
+                  backgroundColor: "#ff008450",
+                  border: "3px solid #ff008450",
+                  fontWeight: "700",
+                  borderRadius: "4px 4px 4px 4px",
+                  marginLeft: "3px",
                 }}
               >
-                x
+                Save
               </button>
-            </li>
-          ))}
-      </ul>
-    </div>
+            </form>
+          </div>
+        </div>
+      </div>
   );
 }
 
---
+++ src/timeDie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TimeDie = () => {
+
+  return (
+    <div className="timeDie">
+    </div>
+  );
+};
+
+export default TimeDie;
