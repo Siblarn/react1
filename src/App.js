99fe43e9b1df03f3718027c428b39409c48c5bcf
siblarn <siblarn5905@gmail.com>
2023-07-05 11:00:23 +0700

    ทดลองยิง

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const data = {
+    username: "Thippawan1234568กข",
+    password: "Siblarn5905",
+    userProfile: {
+      firstName: "Thippawan",
+      lastName: "Puttachart",
+      gender: "Female",
+      birthDate: "2000-02-17",
+      imageUrl: "https://lh3.googleusercontent.com/a/AGNmyxbL_EzNJDYY9thXth0NAUCRiDxlgJF7AUJeEuSXQg=s96-c"
+    },
+    userPhone: {
+      phoneCode: "+66",
+      phoneNumber: "929985905"
+    },
+    userConfigs: {
+      moblieApp: {},
+      webApp: {
+        language: "en",
+        news_modal: true
+      }
+    },
+    userRoles: "admin",
+    isVerified: false,
+    email: "Siblarn5905@gmail.com"
+  };
+  
+  fetch(' http://61.7.237.18:1150/user/new', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Response:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} width="150" className="App-logo" alt="logo" />
-        <h1>Reasons I'm excited to learn React</h1>
-        <ol>
-                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
-      </header>
+      eiei
     </div>
   );
 }
