9f1895b1d01155e33225e4be27b10001f4f9e654
siblarn <siblarn5905@gamil.com>
2023-07-12 16:06:23 +0700

    Homepage Pokemon12/7/66

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import ReactDOM from "react";
 import { BrowserRouter, Form, Route, Routes } from "react-router-dom";
+import Testcard from "./Pokemon/Testcard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/"  element={<Testcard/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/Pokemon/Testcard.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+export default function Testcard() {
+  return (
+    <div>
+      Pokemon
+      <div class="container text-center">
+        <div class="row align-items-start"></div>
+          <div class="col">Name
+          <div className="name1">Bulbasaur</div>
+          <div className="name2">Ivysaur</div>
+          <div className="name3">Charmander</div>
+          <div className="name4">Charizard</div>
+          <div className="name5">MegaBlastoise</div>
+          <div className="name6">Blastoise</div>
+          <div className="name7">Pikachu</div>
+          <div className="name8">Raichu</div>
+          <div className="name9">Sandshrew</div>
+          <div className="name10">Sandslash</div>
+          </div>
+          <div class="col">Type
+          <div className="type1">GRASS,POISON</div>
+          <div className="type2">GRASS,POISON</div>
+          <div className="type3">FIRE</div>
+          <div className="type4">FIRE,FLYING</div>
+          <div className="type5">WATER</div>
+          <div className="type6">WATER</div>
+          <div className="type7">ELECTRIC</div>
+          <div className="type8">ELECTRIC,PSYCHIC</div>
+          <div className="type9">ICE,STEEL</div>
+          <div className="type10">ICE,STEEL</div>
+          </div>
+          <div class="col">Hp
+          <div className="hp1">45</div>
+          <div className="hp2">60</div>
+          <div className="hp3">39</div>
+          <div className="hp4">78</div>
+          <div className="hp5">79</div>
+          <div className="hp6">79</div>
+          <div className="hp7">35</div>
+          <div className="hp8">60</div>
+          <div className="hp9">50</div>
+          <div className="hp10">75</div>
+          </div>
+          <div class="col">Attack
+          <div className="atk1">49</div>
+          <div className="atk2">62</div>
+          <div className="atk3">52</div>
+          <div className="atk4">104</div>
+          <div className="atk5">103</div>
+          <div className="atk6">83</div>
+          <div className="atk7">55</div>
+          <div className="atk8">85</div>
+          <div className="atk9">75</div>
+          <div className="atk10">100</div>
+          </div>
+          <div class="col">Defense
+          <div className="def1">49</div>
+          <div className="def2">63</div>
+          <div className="def3">43</div>
+          <div className="def4">78</div>
+          <div className="def5">120</div>
+          <div className="def6">100</div>
+          <div className="def7">40</div>
+          <div className="def8">50</div>
+          <div className="def9">90</div>
+          <div className="def10">120</div>
+          </div>
+          <div class="col">Sp.Atk
+          <div className="SpAtk1">65</div>
+          <div className="SpAtk2">80</div>
+          <div className="SpAtk3">60</div>
+          <div className="SpAtk4">159</div>
+          <div className="SpAtk5">135</div>
+          <div className="SpAtk6">85</div>
+          <div className="SpAtk7">50</div>
+          <div className="SpAtk8">95</div>
+          <div className="SpAtk9">10</div>
+          <div className="SpAtk10">25</div>
+          </div>
+          <div class="col">Sp.Def
+          <div className="SpDef1">65</div>
+          <div className="SpDef2">80</div>
+          <div className="SpDef3">50</div>
+          <div className="SpDef4">115</div>
+          <div className="SpDef5">115</div>
+          <div className="SpDef6">105</div>
+          <div className="SpDef7">50</div>
+          <div className="SpDef8">85</div>
+          <div className="SpDef9">35</div>
+          <div className="SpDef10">65</div>
+          </div>
+          <div class="col">Speed
+          <div className="speed1">45</div>
+          <div className="speed2">60</div>
+          <div className="speed3">65</div>
+          <div className="speed4">100</div>
+          <div className="speed5">78</div>
+          <div className="speed6">78</div>
+          <div className="speed7">90</div>
+          <div className="speed8">110</div>
+          <div className="speed9">40</div>
+          <div className="speed10">65</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
