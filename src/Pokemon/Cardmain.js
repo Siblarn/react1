076e7cda16a6057dc841abe8d2ac4bcfb5506761
siblarn <siblarn5905@gamil.com>
2023-07-17 14:39:22 +0700

    card

--- src/Pokemon/Cardmain.js
+++ src/Pokemon/Cardmain.js
@@ -1,7 +1,9 @@
 import React from "react";
+import pika from "../image/pika.png";
+import mewtwo from "../image/mewtwo.png";
+import evee from "../image/evee.png";
 
 export default function Cardmain() {
-  
   return (
     <div>
       <h1>Pokemon Card</h1>
@@ -10,35 +12,65 @@ export default function Cardmain() {
 
       <section className="demo">
         <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card" style={{width:'12rem', height:"16.5rem"}}></div>
-        <div className="card" style={{width:'12rem', height:"16.5rem"}}></div>
+        <div
+          className="card"
+          style={{
+            background: "url(https://assets.codepen.io/13471/eevee-gx.webp)",
+          }}
+        ></div>
+        <div
+          className="card"
+          style={{
+            background: "url(https://assets.codepen.io/13471/mewtwo-gx.webp)",
+          }}
+        ></div>
+        <div className="ei3">
+          <img src={evee} width="192rem" alt="evee" />
+        </div>
+        <div className="ei2">
+          <img src={mewtwo} width="192rem" alt="mewtwo" />
+        </div>
+        <div
+          className="card"
+          style={{ width: "12rem", height: "16.5rem" }}
+        ></div>
+        <div className="ei1">
+          <img src={pika} width="192rem" alt="pika" />
+        </div>
+        <div
+          className="card"
+          style={{ width: "12rem", height: "16.5rem" }}
+        ></div>
         <span className="operator"></span>
         <span className="operator"></span>
       </section>
       <div style={{ justifyContent: "center", display: "flex" }}>
-        <div className="" style={{ color: "black", fontWeight: "750" }}>
+        <div className="" style={{ color: "black", fontWeight: "750", margin:"30px"}}>
           Player 1
         </div>
         <style className="hover2"></style>
         <section className="demo2">
-          <div className="card2" style={{width:'12rem', height:"16.5rem"}}></div>
+          <div
+            className="card2"
+            style={{ width: "12rem", height: "16.5rem" }}
+          ></div>
           <span className="operator2"></span>
           <span className="operator2"></span>
         </section>
 
-        <div className="" style={{ color: "black", fontWeight: "750" }}>
-          Player 2
+        <div className="" style={{ color: "black", fontWeight: "750", margin:"30px" }}>
+          Player 2<div className="card10"></div>
         </div>
         <style className="hover2"></style>
         <section className="demo2">
-          <div className="card2" style={{width:'12rem', height:"16.5rem"}}></div>
+          <div
+            className="card2"
+            style={{ width: "12rem", height: "16.5rem" }}
+          ></div>
           <span className="operator2"></span>
           <span className="operator2"></span>
         </section>
       </div>
-
     </div>
   );
 }
