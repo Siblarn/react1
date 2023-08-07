6100d135702485d55686ba6583feabed81204fad
siblarn <siblarn5905@gamil.com>
2023-08-07 13:23:24 +0700

    4/08/66

--- src/Navbar/Nav.js
+++ src/Navbar/Nav.js
@@ -1,23 +1,33 @@
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import eieiicon from "../image/eieiicon.png";
 
 export default function Nav() {
   return (
-    <div>
-    <nav>
-      <ul className="flex">
-        <li>
-          <Link to="/Skill">Pokemon/Skill</Link>
-        </li>
-        <li className="ml-5">
-        <Link to="/PlayPokemon">How To Play</Link>
-        </li>
-        <li>
-          
-        </li>
-      </ul>
-    </nav>
-
-    <Outlet />
-  </div>
+    <div className="">
+      <nav className="navbar bg-body-tertiary">
+        <div className="container">
+          <div
+            className="flex"
+            style={{ justifyContent: "space-between", alignItems: "center" }}
+          >
+            <img src={eieiicon} alt="Bootstrap" width="30" height="24" />
+            <div
+              className="ml-5 flex"
+              style={{ fontFamily: "roboto", fontWeight: "750" }}
+            >
+              POKEMON GAME
+              <div className="ml-10">
+                <Link to="/Skill">Pokemon/Skill</Link>
+              </div>
+              <div className="ml-10">
+                <Link to="/PlayPokemon">How To Play</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
   );
 }
--- src/Play/Battle.js
+++ src/Play/Battle.js
@@ -1,11 +1,20 @@
 import React from "react";
 export default function Battle() {
+  const shoot = () => {
+    alert("Player 1 win!");
+  };
   return (
     <div className="m-4">
       <div className=" bubbly-button">Click for Start</div>
-      <div>Player 1 VS Player 2
-      </div>
+      <div>Player 1 VS Player 2</div>
+      <div className="flex space-x-10 mt-4">
         <div className="button2">Round 1</div>
+        <div>
+          <button onClick={shoot}>
+            <div className="button2">Finish</div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
