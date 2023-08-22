f88cd1804892f52a6ce558a485da0cef6e8147dc
siblarn <siblarn5905@gamil.com>
2023-08-22 15:37:05 +0700

    22/8/66

--- src/Test/Check.js
+++ src/Test/Check.js
@@ -3,19 +3,40 @@ import Active from "./Active";
 import Todolist from "./Todolist";
 
 export default function Check() {
-    return (
+  return (
     <div>
-        <div>
-            ♥ Check Work!
-            <>
-         <Todolist name="Work 1 : ลายละเอียดงานที่Staffส่งมา"/>
-         <Todolist name="Work 2 : ลายละเอียดงานที่Staffส่งมา"/>
-         <Todolist name="Work 3 : ลายละเอียดงานที่Staffส่งมา"/>
-            </>
-        </div>
-
-       
+      <div>
+        ♥ Check Work!
+        <>
+          <div>
+            <Todolist name="Work 1 : ลายละเอียดงานที่Staffส่งมา" />
+            <button type="submit" className="btn toggle-btn btn__sm">
+              Complete
+            </button>
+            <button type="submit" className="btn toggle-btn btn__sm">
+              cancel
+            </button>
+          </div>
+          <div>
+            <Todolist name="Work 2 : ลายละเอียดงานที่Staffส่งมา" />
+            <button type="submit" className="btn toggle-btn btn__sm">
+              Complete
+            </button>
+            <button type="submit" className="btn toggle-btn btn__sm">
+              cancel
+            </button>
+          </div>
+          <div>
+            <Todolist name="Work 3 : ลายละเอียดงานที่Staffส่งมา" />
+            <button type="submit" className="btn toggle-btn btn__sm">
+              Complete
+            </button>
+            <button type="submit" className="btn toggle-btn btn__sm">
+              cancel
+            </button>
+          </div>
+        </>
+      </div>
     </div>
-    );
-  }
-  
+  );
+}
